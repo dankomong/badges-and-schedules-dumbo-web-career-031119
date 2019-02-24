3409fa74ac7ee8f38bb17884edafc9cb4778b441
Daniel Ko <dankomong@gmail.com>
2019-02-24 23:57:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(array)
   new_array = []
   array.each_with_index do |val, index|
-    "Hello, #{elem}! You'll be assigned to room #{index + 1}!"
+    "Hello, #{val}! You'll be assigned to room #{index + 1}!"
     new_array.push(phrase)
   end
   new_array
